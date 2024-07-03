98fb67a91e046ecbb3ed3104235112c24d572b1e
Marcelo Soares <marcelohsoares142@gmail.com>
2024-07-03 06:10:39 -0300

    usando o plugin watch do Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,10 +15,17 @@ module.exports = function (grunt) {
                 'dist/styles/main.min.css': "src/styles/main.less"    
             }
         }
+    },
+    watch: {
+        less: {
+            files: ["src/styles/**/*.less"],   // ** qualquer pasta    &   * qualquer arquivo
+            tasks: ["less:development"]
+        }
     }
     });
     grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-watch");
 
-    grunt.registerTask("default", ["less:development"]);
+    grunt.registerTask("default", ["watch"]);
     grunt.registerTask("build", ["less:production"]);
 };
